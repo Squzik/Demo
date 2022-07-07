fb4cdc50a278d2bb7460e6547ad9d919d989ce06
iwzxcv69 <iwzxcv69@gmail.com>
2022-07-07 12:06:21 +0300

    Изменил подключение

--- src/test/java/com/example/demo/HttpUrlConnectionExample.java
+++ src/test/java/com/example/demo/HttpUrlConnectionExample.java
@@ -28,15 +28,15 @@ public class HttpUrlConnectionExample {
 
 		public void main() throws Exception {
 
-			String url = "https://askubuntu.com/users/login";
-			String askubuntu = "https://askubuntu.com/users/1610836/squzik";
+			String url = "https://account.habr.com/login/?state=b19ae298dac772ddbe1784898b43512f&consumer=habr&hl=ru_RU";
+			String habr = "https://habr.com/ru/users/Squzik/";
 			HttpUrlConnectionExample http = new HttpUrlConnectionExample();
 			CookieHandler.setDefault(new CookieManager());
 			String page = http.GetPageContent(url);
 			System.out.println(page);
-			String postParams = http.getFormParams(page, "Lucaspw@yandex.ru", "iy5-X34-mZ2-gZY");
+			String postParams = http.getFormParams(page, "Lucaspw@yandex.ru", "Lucaspw123123");
 			http.sendPost(url, postParams);
-			String result = http.GetPageContent(askubuntu);
+			String result = http.GetPageContent(habr);
 			System.out.println(result);
 		}
 
@@ -46,16 +46,16 @@ public class HttpUrlConnectionExample {
 			conn = (HttpsURLConnection) obj.openConnection();
 			conn.setUseCaches(false);
 			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Host", "accounts.google.com");
+			conn.setRequestProperty("Host", "account.habr.com");
 			conn.setRequestProperty("User-Agent", USER_AGENT);
 			conn.setRequestProperty("Accept",
-					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+					"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+			conn.setRequestProperty("Accept-Language", " ru,en;q=0.9");
 			for (String cookie : this.cookies) {
 				conn.addRequestProperty("Cookie", cookie.split(";", 1)[0]);
 			}
 			conn.setRequestProperty("Connection", "keep-alive");
-			conn.setRequestProperty("Referer", "https://askubuntu.com/users/login");
+			conn.setRequestProperty("Referer", "https://habr.com/ru/all/");
 			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 			conn.setRequestProperty("Content-Length", Integer.toString(postParams.length()));
 			conn.setDoOutput(true);
@@ -80,7 +80,6 @@ public class HttpUrlConnectionExample {
 				response.append(inputLine);
 			}
 			in.close();
-
 		}
 
 		private String GetPageContent(String url) throws Exception {
@@ -126,16 +125,16 @@ public class HttpUrlConnectionExample {
 
 			Document doc = Jsoup.parse(html);
 
-			Element loginform = doc.getElementById("login-form");
+			Element loginform = doc.getElementById("login_form");
 			Elements inputElements = loginform.getElementsByTag("input");
 			List<String> paramList = new ArrayList<String>();
 			for (Element inputElement : inputElements) {
 				String key = inputElement.attr("name");
 				String value = inputElement.attr("value");
 				System.out.println("key = " + key + " , val = " + value);
-				if (key.equals("email"))
+				if (key.equals("email_field"))
 					value = username;
-				else if (key.equals("password"))
+				else if (key.equals("password_field"))
 					value = password;
 				paramList.add(key + "=" + URLEncoder.encode(value, StandardCharsets.UTF_8));
 			}
